f2704156e4cc92dbc190f66f9d029d9a4dc12108
ashleyvasquez-web <ashley.vasquez@est.ulsa.edu.ni>
2026-02-17 22:34:24 -0600

    edit

--- horarioscropt.js
+++ horarioscropt.js
@@ -8,17 +8,34 @@ btnLista.addEventListener("click", () => {
   vistaLista.classList.remove("d-none");
   vistaCalendario.classList.add("d-none");
 
-  btnLista.classList.replace("btn-outline-secondary", "btn-primary");
-  btnCalendario.classList.replace("btn-primary", "btn-outline-secondary");
+  btnLista.classList.add("btn-primary");
+  btnLista.classList.remove("btn-outline-secondary");
+
+  btnCalendario.classList.remove("btn-primary");
+  btnCalendario.classList.add("btn-outline-secondary");
 });
 
 btnCalendario.addEventListener("click", () => {
   vistaLista.classList.add("d-none");
   vistaCalendario.classList.remove("d-none");
 
-  btnCalendario.classList.replace("btn-outline-secondary", "btn-primary");
-  btnLista.classList.replace("btn-primary", "btn-outline-secondary");
+  btnCalendario.classList.add("btn-primary");
+  btnCalendario.classList.remove("btn-outline-secondary");
+
+  btnLista.classList.remove("btn-primary");
+  btnLista.classList.add("btn-outline-secondary");
 });
+
+/* ===== DATOS DE CLASES ===== */
+
+const clases = [
+  { dia: "Martes", hora: 10, nombre: "Programación Web", grupo: "Gpo 1" },
+  { dia: "Jueves", hora: 15, nombre: "Programación Web", grupo: "Gpo 1" },
+  { dia: "Lunes", hora: 8, nombre: "Intro. Ingeniería", grupo: "Gpo 3" },
+  { dia: "Miércoles", hora: 8, nombre: "Intro. Ingeniería", grupo: "Gpo 3" },
+  { dia: "Lunes", hora: 10, nombre: "Intro. Ingeniería", grupo: "Gpo 4" },
+  { dia: "Jueves", hora: 8, nombre: "Intro. Ingeniería", grupo: "Gpo 4" }
+];
 
 const dias = ["Hora", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
 
@@ -28,12 +45,28 @@ thead += "</tr></thead>";
 
 let tbody = "<tbody>";
 
-for (let hora = 8; hora <= 14; hora++) {
+for (let hora = 8; hora <= 16; hora++) {
+
   tbody += `<tr>`;
-  tbody += `<td>${hora}:00</td>`;
+  tbody += `<td class="fw-bold">${hora}:00</td>`;
 
   for (let d = 1; d < dias.length; d++) {
-    tbody += `<td></td>`; // celda vacía OBLIGATORIA
+
+    const clase = clases.find(c => c.dia === dias[d] && c.hora === hora);
+
+    if (clase) {
+      tbody += `
+        <td>
+          <div class="bg-primary text-white rounded p-2 small">
+            ${clase.nombre}<br>
+            ${clase.grupo}
+          </div>
+        </td>
+      `;
+    } else {
+      tbody += `<td></td>`; // CELDA VACÍA OBLIGATORIA
+    }
+
   }
 
   tbody += `</tr>`;
